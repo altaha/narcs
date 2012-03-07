b92edcd3e0fbda5561e12b9433f049e5117d581c
adeel.raza <adeel.raza@164d3ad6-1dc4-4cc4-b508-18e33b004436>
2012-03-07 21:14:11 +0000

    - Added correct "trunk/Socket Connectivity Test Code/client.cpp"
    git-svn-id: https://xp-dev.com/svn/NARCSRepository@10 164d3ad6-1dc4-4cc4-b508-18e33b004436

--- trunk/Socket Connectivity Test Code/client.cpp
+++ trunk/Socket Connectivity Test Code/client.cpp
@@ -1,122 +1,112 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <stdio.h>
 #include <iostream>
+
 
 #pragma comment(lib, "Ws2_32.lib")
 
-#define DEFAULT_PORT "62009"
+
+#define SERVER_IP_ADDRESS_BYTE_1  172
+#define SERVER_IP_ADDRESS_BYTE_2  21
+#define SERVER_IP_ADDRESS_BYTE_3  19
+#define SERVER_IP_ADDRESS_BYTE_4  242
+#define SERVER_PORT  62009
 #define DEFAULT_BUFLEN 512
+
 
 using namespace std;
 
+
 int main() {
+	WSADATA wsaData;
+	int iResult;
 
-	char recvbuf[DEFAULT_BUFLEN];
-	int iResult, iSendResult;
-
-	WSADATA wsaData;	//to inialize Windows socket dll
-	struct addrinfo *result=NULL, *ptr=NULL, hints;
-	SOCKET ListenSocket = INVALID_SOCKET, ClientSocket = INVALID_SOCKET;
-
-
-	//Initialize Winscok
+	// Initialize Winsock
 	iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
-	if (iResult) {
+	if (iResult != 0) {
 		printf("WSAStartup failed: %d\n", iResult);
 		return 1;
 	}
 
-	//Creating socker IP address
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
 
-	//resolve the local address and port to be used by the server
-	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-	if (iResult) {
-		printf("getaddrinfofailed: %d\n", iResult);
-		WSACleanup();
-		return 1;
-	}
-	
-	//Create a SOCKET for the server to listen for client connections
-	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-
-	if (ListenSocket == INVALID_SOCKET) {
+	SOCKET ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (ConnectSocket == INVALID_SOCKET)
+	{
 		printf("Error at socket(): %ld\n", WSAGetLastError());
-		freeaddrinfo(result);
 		WSACleanup();
 		return 1;
 	}
 
-	//Setup the TCP listening socket
-	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-	if (iResult == SOCKET_ERROR) {
-		printf("bind failed with error: %d\n", WSAGetLastError());
-		freeaddrinfo(result);
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
 
-	freeaddrinfo(result);	// no longer need the address information
-
-	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
-		printf("Listen failed with error: %ld\n", WSAGetLastError());
-		closesocket(ListenSocket);
-		WSACleanup();
-		return 1;
-	}
+	struct sockaddr_in serverAddress;
 	
-	printf("All good. Waiting for request\n");
-	//Accept a client socket
-	ClientSocket = accept(ListenSocket, NULL, NULL);
-	if (ClientSocket == INVALID_SOCKET) {
-		printf("accept failed: %d\n", WSAGetLastError());
-		closesocket(ListenSocket);
+	ZeroMemory(&serverAddress, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(SERVER_PORT);
+	//serverAddress.sin_addr.S_un.S_addr = htonl(SERVER_IP_ADDRESS_BYTE_4 | SERVER_IP_ADDRESS_BYTE_3 | SERVER_IP_ADDRESS_BYTE_2 | SERVER_IP_ADDRESS_BYTE_1);
+	serverAddress.sin_addr.S_un.S_un_b.s_b1 = (unsigned char)(SERVER_IP_ADDRESS_BYTE_1);
+	serverAddress.sin_addr.S_un.S_un_b.s_b2 = (unsigned char)(SERVER_IP_ADDRESS_BYTE_2);
+	serverAddress.sin_addr.S_un.S_un_b.s_b3 = (unsigned char)(SERVER_IP_ADDRESS_BYTE_3);
+	serverAddress.sin_addr.S_un.S_un_b.s_b4 = (unsigned char)(SERVER_IP_ADDRESS_BYTE_4);
+
+	iResult = connect(ConnectSocket, (const struct sockaddr *)(&serverAddress), sizeof(serverAddress));
+	if (iResult == SOCKET_ERROR) {
+		closesocket(ConnectSocket);
+		printf("Unable to connect to server: %ld\n", WSAGetLastError());
 		WSACleanup();
 		cin.get();
 		return 1;
 	}
 
-	printf("Accept succeeded\n\n");
+	printf("Successfull Connection!\n\n");
 
-	
-	// Receive until the peer shuts down the connection
-	do {
-		iResult = recv(ClientSocket, recvbuf, DEFAULT_BUFLEN, 0);
-		if (iResult > 0) {
-			printf("Bytes received: %d\n", iResult);
-			recvbuf[iResult] = NULL;
-			printf("recvbuf = \"%s\"\n\n", recvbuf);
 
-			// Echo the buffer back to the sender
-			iSendResult = send(ClientSocket, recvbuf, iResult, 0);
-			if (iSendResult == SOCKET_ERROR) {
-				printf("send failed: %d\n", WSAGetLastError());
-				closesocket(ClientSocket);
-				WSACleanup();
-				return 1;
-			}
-			printf("Bytes sent: %d\n", iSendResult);
-			printf("sendbuf = \"%s\"\n\n", recvbuf);
-		} else if (iResult == 0)
-			printf("Connection closing...\n");
-		else {
-			printf("recv failed: %d\n", WSAGetLastError());
-			closesocket(ClientSocket);
-			WSACleanup();
-			return 1;
-		}
+	char *sendbuf = "this is a test";
+	char recvbuf[DEFAULT_BUFLEN];
 
-	} while (iResult > 0);
+	// Send an initial buffer
+	iResult = send(ConnectSocket, sendbuf, (int) strlen(sendbuf), 0);
+	if (iResult == SOCKET_ERROR) {
+		printf("send failed: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		cin.get();
+		return 1;
+	}
 
-	closesocket(ClientSocket);
+	printf("Bytes Sent: %ld\n", iResult);
+	printf("sendbuf = \"%s\"\n", sendbuf);
+
+	// shutdown the connection for sending since no more data will be sent
+	// the client can still use the ConnectSocket for receiving data
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		cin.get();
+		return 1;
+	}
+
+	cout<<"Sending suceeded!"<<endl<<endl;
+
+
+	// Receive data until the server closes the connection
+	iResult = recv(ConnectSocket, recvbuf, DEFAULT_BUFLEN, 0);
+	if (iResult > 0) {
+		printf("Bytes received: %d\n", iResult);
+        recvbuf[iResult] = NULL;
+		printf("recvbuf = \"%s\"\n\n", recvbuf);
+	}
+	else if (iResult == 0)
+		printf("Connection closed\n");
+	else
+		printf("recv failed: %d\n", WSAGetLastError());
+
+
+	closesocket(ConnectSocket);
 	WSACleanup();
-    cin.get();
+	printf("Closing connection\n");
+	cin.get();
 	return 0;
 }
-
