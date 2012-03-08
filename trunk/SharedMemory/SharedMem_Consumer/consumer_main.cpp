9a462b73cd05476689d84f3deb6fd37072ed87a6
Ahmed Altaha <ahmedsalam.2008@gmail.com>
2012-03-08 05:03:40 +0000

    Added synchronization between producer and consumer using an Event and a Mutex -Works pretty well
    git-svn-id: https://xp-dev.com/svn/NARCSRepository@19 164d3ad6-1dc4-4cc4-b508-18e33b004436

--- trunk/SharedMemory/SharedMem_Consumer/consumer_main.cpp
+++ trunk/SharedMemory/SharedMem_Consumer/consumer_main.cpp
@@ -31,9 +31,56 @@ int main(){
 	printf("Size of struct: %d\n", STRUCT_SIZE);
 	printf("Size of memory is: %d\n", sharedMemory.GetSize() );
 
-	joint_data read_test;
-	if( sharedMemory.Read( (void*)&read_test, STRUCT_SIZE, 5) ){
-		printf("I read x:%f, y:%f, z:%f\n", read_test.x, read_test.y, read_test.z);
+
+	////Event and Mutex Objects
+	HANDLE ghWriteEvent = OpenEvent(
+        SYNCHRONIZE,		//Standard access
+        FALSE,				//Child processes dont inherit
+        TEXT("SharedMemWriteEvent")  // object name
+        );
+	if (ghWriteEvent == NULL) 
+    { 
+        printf("CreateEvent failed (%d)\n", GetLastError());
+        return 1;
+    }
+
+	HANDLE ghMutex = OpenMutex(
+        SYNCHRONIZE,       //Standard access
+        FALSE,             //Child processes dont inherit
+        TEXT("SharedMemMutex")  // object name
+		);
+    if (ghMutex == NULL) 
+    {
+        printf("CreateMutex error: %d\n", GetLastError());
+        return 1;
+    }
+	////////////////////////////////
+	int loop_count = 0;
+	while(loop_count < 1000)
+	{
+		HANDLE lpHandles[2];
+		lpHandles[0] = ghWriteEvent;
+		//lpHandles[0] = ghMutex;
+		lpHandles[1] = ghMutex;
+		DWORD rc = WaitForMultipleObjects(2, lpHandles, TRUE, 20000);
+	//	DWORD rc = WaitForSingleObject(ghMutex, 10000);
+		if(rc != WAIT_OBJECT_0){
+			printf("WaitForMultipleObjects error: %d\n", GetLastError());
+			return 1;
+		}
+
+		joint_data read_test;
+		if( sharedMemory.Read( (void*)&read_test, STRUCT_SIZE, 5) ){
+			printf("I read x:%f, y:%f, z:%f\n", read_test.x, read_test.y, read_test.z);
+		}
+
+		if( !ReleaseMutex( ghMutex) )
+		{
+			printf("ReleaseMutex failed (%d)\n", GetLastError());
+			return 1;
+		}
+		loop_count++;
+		printf("Consumer Loop: %d\n",loop_count);
 	}
 
 	wprintf(sharedMemory.GetName());
@@ -41,5 +88,7 @@ int main(){
 	//terminate
 	printf("Process 2 done\n");
 	getchar();
+	CloseHandle(ghWriteEvent);
+	CloseHandle(ghMutex);
 	return 0;
 }
--- trunk/SharedMemory/SharedMem_Producer/producer_main.cpp
+++ trunk/SharedMemory/SharedMem_Producer/producer_main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <Windows.h>
+#include <WinBase.h>
 
 #include "SharedMem.h"
 
@@ -28,10 +29,74 @@ int main(){
 
 	joint_data test_data = { 0.5, 0.19, 3.2};
 
-	sharedMemory.Write((void*)&test_data, STRUCT_SIZE, 5);
+	//Create Event and Mutex Objects for synchronization
+	HANDLE ghWriteEvent = CreateEvent( 
+        NULL,               // default security attributes
+        FALSE,               // automatic-reset event
+        FALSE,              // initial state is nonsignaled
+        TEXT("SharedMemWriteEvent")  // object name
+        );
+	if (ghWriteEvent == NULL) 
+    { 
+        printf("CreateEvent failed (%d)\n", GetLastError());
+        return 1;
+    }
+
+	HANDLE ghMutex = CreateMutex( 
+        NULL,              // default security attributes
+        FALSE,             // initially owned
+        TEXT("SharedMemMutex")  // object name
+		);
+    if (ghMutex == NULL) 
+    {
+        printf("CreateMutex error: %d\n", GetLastError());
+        return 1;
+    }
+	////////////////////
+
+	int loop_count = 0;
+	printf("Hit a Key to start cycle\n");
+	getchar();
+	while(loop_count < 1000)
+	{
+		DWORD dwWaitResult = WaitForSingleObject( 
+				ghMutex,    // handle to mutex
+				INFINITE);  // no time-out interval
+		switch (dwWaitResult) 
+		{
+			// The thread got ownership of the mutex
+			case WAIT_OBJECT_0: 
+				printf("Acquired Mutex!!!!!\n");
+				sharedMemory.Write((void*)&test_data, STRUCT_SIZE, 5);
+				break; 
+
+			// The thread got ownership of an abandoned mutex
+			// The database is in an indeterminate state
+			default: 
+				break; 
+		}
+		if (! SetEvent(ghWriteEvent) ) 
+		{
+			printf("SetEvent failed (%d)\n", GetLastError());
+			return 1;
+		}
+		if( !ReleaseMutex( ghMutex) )
+		{
+			printf("ReleaseMutex failed (%d)\n", GetLastError());
+			return 1;
+		}
+		loop_count++;
+		printf("Producer Loop: %d\n",loop_count);
+		test_data.x += 1;
+		test_data.y += 1;
+		test_data.z += 1;
+		Sleep(50);
+	}
 
 	//terminate
 	printf("Creator process done\n");
 	getchar();
+	CloseHandle(ghWriteEvent);
+	CloseHandle(ghMutex);
 	return 0;
 }	
