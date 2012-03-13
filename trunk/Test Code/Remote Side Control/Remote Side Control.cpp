9b70b7863d93e0019c65247e14123fc1f48547e3
adeel.raza <adeel.raza@164d3ad6-1dc4-4cc4-b508-18e33b004436>
2012-03-13 06:44:08 +0000

    - "trunk/Test Code/Remote Side Control/Remote Side Control.cpp":- Implemented on-demand position update
    git-svn-id: https://xp-dev.com/svn/NARCSRepository@45 164d3ad6-1dc4-4cc4-b508-18e33b004436

--- trunk/Test Code/Remote Side Control/Remote Side Control.cpp
+++ trunk/Test Code/Remote Side Control/Remote Side Control.cpp
@@ -3,17 +3,15 @@
 #include <ws2tcpip.h>
 #include <stdio.h>
 #include <iostream>
-#include <MMSystem.h>
 
 
 #pragma comment(lib, "Ws2_32.lib")
-#pragma comment(lib, "winmm.lib")
 #import "C:\Program Files\CRS Robotics\ActiveRobot\ActiveRobot.dll"
 
 
 #define DEFAULT_PORT  "62009"
 #define ARM_SPEED  80
-#define POISTION_UPDATE_BUFFER_LENGTH  12
+#define POSITION_UPDATE_BUFFER_LENGTH  12
 //#define ADEEL_DEBUG
 
 #ifdef ADEEL_DEBUG
@@ -62,9 +60,7 @@ int main() {
 
 	try
 	{
-		locFlags flags = readyPosition->GetFlags();
-		flags = (locFlags)(flags | locBase);
-		endEffectorPosition->PutFlags(flags);
+		endEffectorPosition->PutFlags(readyPosition->GetFlags());
 		endEffectorPosition->PutClass(readyPosition->GetClass());
 		endEffectorPosition->PutRobotType(locRCA255);
 	}
@@ -162,29 +158,71 @@ int main() {
 	
 
 	
-	char position_update_buffer[POISTION_UPDATE_BUFFER_LENGTH];
+	char position_update_buffer[POSITION_UPDATE_BUFFER_LENGTH];
 	int curr_bytes_read = -1;
 	unsigned int total_bytes_read = 0;
 	char *curr_buffer_ptr = position_update_buffer;
 	float endEffectorX,
 		  endEffectorY,
 		  endEffectorZ;
-	
+	unsigned int totalBytesWritten = 0;
+	int currBytesWritten = -1;
+
 #ifdef ADEEL_DEBUG
 	int moveStartTime;
 	fstream debugOut("debug.txt", ios::out);
 #endif
 	while(true)
 	{
-		// Read main message contents
+		// Send the message
+		curr_buffer_ptr = position_update_buffer;
+		totalBytesWritten = 0;
+		currBytesWritten = -1;
+		while(totalBytesWritten < 1)
+		{
+			currBytesWritten = send(ClientSocket,
+									curr_buffer_ptr,
+									1 - totalBytesWritten,
+									0);
+			if (currBytesWritten == 0)
+			{
+				printf("Connection closing...\n");
+				closesocket(ClientSocket);
+				WSACleanup();
+				Robot->ControlRelease();
+				cin.get();
+#ifdef ADEEL_DEBUG
+				debugOut.close();
+#endif
+				return 0;
+			}
+			else if (currBytesWritten < 0)
+			{
+				printf("send failed: %d\n", WSAGetLastError());
+				closesocket(ClientSocket);
+				WSACleanup();
+				Robot->ControlRelease();
+				cin.get();
+#ifdef ADEEL_DEBUG
+				debugOut.close();
+#endif
+				return -1;
+			}
+
+			totalBytesWritten += currBytesWritten;
+			curr_buffer_ptr += currBytesWritten;
+		}
+
+
+		// Receive position update
 		curr_bytes_read = -1;
 		total_bytes_read = 0;
 		curr_buffer_ptr = position_update_buffer;
-		while(total_bytes_read < POISTION_UPDATE_BUFFER_LENGTH)
+		while(total_bytes_read < POSITION_UPDATE_BUFFER_LENGTH)
 		{
 			curr_bytes_read = recv(ClientSocket,
 								   curr_buffer_ptr,
-								   POISTION_UPDATE_BUFFER_LENGTH - total_bytes_read,
+								   POSITION_UPDATE_BUFFER_LENGTH - total_bytes_read,
 								   0);
 			if (curr_bytes_read == 0)
 			{
@@ -238,13 +276,11 @@ int main() {
 			endEffectorPosition->Putx(endEffectorX);
 			endEffectorPosition->Puty(endEffectorY);
 			endEffectorPosition->Putz(endEffectorZ);
-			//Robot->Stop();
 #ifdef ADEEL_DEBUG
 			moveStartTime = timeGetTime();
 #endif
-			Robot->Stop();
-			Robot->Move(endEffectorPosition);
-			//Robot->Finish(ftLoose);
+			Robot->MoveStraight(endEffectorPosition);
+			Robot->Finish(ftLoose);
 #ifdef ADEEL_DEBUG
 			debugOut << timeGetTime() - moveStartTime << endl;
 			debugOut.flush();
@@ -266,123 +302,6 @@ int main() {
 		}
 	}
 
-	/*
-	// Receive until the peer shuts down the connection
-	do {
-		char recvbuf[DEFAULT_BUFLEN];
-		iResult = recv(ClientSocket, recvbuf, DEFAULT_BUFLEN, 0);
-		if (iResult > 0) {
-			printf("Bytes received: %d\n", iResult);
-			recvbuf[iResult] = NULL;
-			printf("recvbuf = \"%s\"\n\n", recvbuf);
-
-			// Echo the buffer back to the sender
-			iSendResult = send(ClientSocket, recvbuf, iResult, 0);
-			if (iSendResult == SOCKET_ERROR) {
-				printf("send failed: %d\n", WSAGetLastError());
-				closesocket(ClientSocket);
-				WSACleanup();
-				return -1;
-			}
-			printf("Bytes sent: %d\n", iSendResult);
-			printf("sendbuf = \"%s\"\n\n", recvbuf);
-		} else if (iResult == 0)
-			printf("Connection closing...\n");
-		else {
-			printf("recv failed: %d\n", WSAGetLastError());
-			closesocket(ClientSocket);
-			WSACleanup();
-			return -1;
-		}
-
-	} while (iResult > 0);
 	
-	closesocket(ClientSocket);
-	WSACleanup();
-	cin.get();
-	*/
-
-	/*
-	if FAILED(CoInitialize(NULL))
-	{
-		AfxMessageBox("CoInitialize() failed.");
-		exit(1);
-	}
-
-	ICRSRobotPtr Robot =
-					ICRSRobotPtr(__uuidof(CRSRobot));
-	ICRSLocationPtr locA = 
-					ICRSLocationPtr(__uuidof(CRSLocation));
-
-	try
-	{
-		Robot->ControlGet();
-		ICRSLocationPtr tempLoc = Robot->GetWorldLocation(
-					ACTIVEROBOTLib::ptPositionType::ptActual);
-		cout << "tempLoc->Getx() = " << tempLoc->Getx() << endl
-			 << "tempLoc->Gety() = " << tempLoc->Gety() << endl
-			 << "tempLoc->Getz() = " << tempLoc->Getz() << endl
-			 << "tempLoc->Getxrot() = " << tempLoc->Getxrot()  << endl
-			 << "tempLoc->Getyrot() = " << tempLoc->Getyrot() << endl
-			 << "tempLoc->Getzrot() = " << tempLoc->Getzrot() << endl;
-
-		tempLoc->Getx();
-
-
-
-		stringstream coordinateStream(recvbuf);
-		string xCoordString, yCoordString, zCoordString;
-		
-		coordinateStream >> xCoordString;
-		coordinateStream >> yCoordString;
-		coordinateStream >> zCoordString;
-
-		stringstream xCoordStringToNumConverter(xCoordString),
-					 yCoordStringToNumConverter(yCoordString),
-					 zCoordStringToNumConverter(zCoordString);
-		double xCoord, yCoord, zCoord;
-
-		xCoordStringToNumConverter >> xCoord;
-		yCoordStringToNumConverter >> yCoord;
-		zCoordStringToNumConverter >> zCoord;
-
-
-		//locA->Putx(280); // 304.778
-		//locA->Puty(20);	// -0.0265
-		//locA->Putz(550);  // 507.998
-		locA->Putx(xCoord); // 304.778
-		locA->Puty(yCoord);	// -0.0265
-		locA->Putz(zCoord);  // 507.998
-		locA->Putxrot(0);
-		locA->Putyrot(0);
-		locA->Putzrot(0);
-		locA->PutFlags(tempLoc->GetFlags());
-		locA->PutClass(
-				ACTIVEROBOTLib::locClass::locCartesian);
-		Robot->Move(locA);
-
-		/*
-		Robot->GripperOpen(50);
-		Robot->GripperFinish();
-		Robot->GripperClose(30);
-		Robot->GripperFinish();
-		//Robot->GripperDistance = 50;
-		//Robot->GripperFinish();
-		
-		Robot->GripperStop();
-		
-	}
-	catch (_com_error MyError)
-	{
-		char WorkString[255];
-		sprintf(WorkString, "The following error occurred during initialization --\n%s", (LPCTSTR) MyError.Description());
-		AfxMessageBox(WorkString);
-		Robot->ControlRelease();
-		exit(1);
-	}
-
-	Robot->ControlRelease();
-	*/
-
 	return 0;
 }
